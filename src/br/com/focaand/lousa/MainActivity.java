34963105e6fc5a48ce1bda5b98326e3f28bce08b
codigoalvo <codigoalvo@gmail.com>
2013-12-12 14:49:49 -0200

    Custon font (OpenDyslexya)

--- src/br/com/focaand/lousa/MainActivity.java
+++ src/br/com/focaand/lousa/MainActivity.java
@@ -3,8 +3,10 @@ package br.com.focaand.lousa;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Typeface face = Typeface.createFromAsset(getAssets(), "fonts/OpenDyslexicAlta-Regular.otf");
+		TextView lblTitle = (TextView)findViewById(R.id.lblTitle);
+		lblTitle.setTypeface(face);
 	}
 
 	@Override
